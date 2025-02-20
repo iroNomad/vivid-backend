2ac3776edd498e3f7b3b125d034bcdc4efd6824a
jaka0926 <jaka0926@naver.com>
2025-02-20 18:01:53 +0900

    refactor

--- src/main/java/com/ironnomad/vivid/repository/VideoRepository.java
+++ src/main/java/com/ironnomad/vivid/repository/VideoRepository.java
@@ -3,6 +3,5 @@ package com.ironnomad.vivid.repository;
 import com.ironnomad.vivid.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VideoRepository extends JpaRepository<Video, Integer> {
-    Video findById(Long id);
+public interface VideoRepository extends JpaRepository<Video, Long> {
 }
--- src/main/java/com/ironnomad/vivid/service/VideoService.java
+++ src/main/java/com/ironnomad/vivid/service/VideoService.java
@@ -11,8 +11,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class VideoService {
@@ -45,6 +43,6 @@ public class VideoService {
     }
 
     public Video getVideoById(Long id) {
-        return videoRepository.findById(id);
+        return videoRepository.findById(id).get();
     }
 }
