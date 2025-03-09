f3ed14c9b7c557fb0b2f1121a91b14b771f8ad86
Jaka Buteelch <89756508+iroNomad@users.noreply.github.com>
2025-03-09 15:21:27 +0900

    .gitignore 수정

--- src/main/java/com/ironnomad/vivid/security/SecurityConfig.java
+++ src/main/java/com/ironnomad/vivid/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ironnomad.vivid.security;
 
-import com.ironnomad.vivid.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
